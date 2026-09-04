f974424eabc744291740a55b8b4ac7ae3a90a28c
MJ Mlambo <156383631+notmjm@users.noreply.github.com>
2026-09-04 17:58:36 -0400

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if (len(s) != len(t)):
+            return False
+        
+        smap = [0] * 26
+        tmap = [0] * 26
+
+        for i in range(len(s)):
+            smap[ord("a") - ord(s[i])] += 1
+            tmap[ord("a") - ord(t[i])] += 1
+        
+        return smap == tmap
+
+        
